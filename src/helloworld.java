e1c12527ade7ad964deacb098af582011adb8247
akostyko <34099073+akostyko@users.noreply.github.com>
2017-11-29 18:18:06 +0300

    add yml

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,5 +3,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Travis checks it");
+        System.out.println("Add yml file");
     }
 }
