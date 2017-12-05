ee09ade9875406894efc9116727aee3926da6d29
akostyko <34099073+akostyko@users.noreply.github.com>
2017-12-05 13:15:48 +0300

    Add build.xml
    Add build.xml

--- src/helloworld.java
+++
@@ -1,8 +0,0 @@
-
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Travis checks it");
-        System.out.println("Add yml file");
-    }
-}
